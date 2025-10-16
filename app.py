3604b188144d482ce6240289fe877d61dc3783ba
KKtuss <jtronville@asteeriks.com>
2025-10-17 01:38:30 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -411,5 +411,5 @@ def get_html_content():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", "8000"))
     uvicorn.run(app, host="0.0.0.0", port=port)
